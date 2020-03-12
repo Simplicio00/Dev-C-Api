70296737cce5271f0a2859b25f90d57d0d645a64
Lucas Simplicio de Oliveira <47944333873@info.local>
2020-03-12 11:29:12 -0300

    api gufi a15 senai

---
+++ 01 Gufi/Gufi.Senai.WebApi/Gufi.Senai.WebApi/Interfaces/ITipoEvento.cs
@@ -0,0 +1,21 @@
+﻿using Gufi.Senai.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gufi.Senai.WebApi.Interfaces
+{
+    interface ITipoEvento
+    {
+        List<TipoEvento> Listar();
+
+        TipoEvento Cadastrar(TipoEvento tipo);
+
+        TipoEvento Atualizar(int id, TipoEvento tipo);
+
+        TipoEvento Deletar(int id);
+
+        TipoEvento BuscarPorId(int id);
+    }
+}
